bd4cd0276cf90225eb63dad84e74d2deda07266a
dallenopenshaw <dallen.openshaw@gmail.com>
2025-04-19 15:34:42 -0600

    ready for deployment now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 
 
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 
 import Home from './pages/Home';
