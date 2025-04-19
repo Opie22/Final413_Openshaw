392b1b6e02b0f09c2066ae52abb907cc6a83e99e
dallenopenshaw <dallen.openshaw@gmail.com>
2025-04-19 13:20:53 -0600

    2nd push

--- backend/FinalExam413_Dallen_Openshaw.API/obj/Debug/net8.0/FinalExam413_Dallen_Openshaw.API.AssemblyInfo.cs
+++ backend/FinalExam413_Dallen_Openshaw.API/obj/Debug/net8.0/FinalExam413_Dallen_Openshaw.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalExam413_Dallen_Openshaw.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b257a37f0dd783e979f61827e9d37cf8ad3c46c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalExam413_Dallen_Openshaw.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalExam413_Dallen_Openshaw.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
